008362f2e9f97f1224e328fc1fdf4f2c91cc8296
ext.roberts.karlis.sa.smits@valtion.onmicrosoft.com <ext.roberts.karlis.sa.smits@valtion.onmicrosoft.com>
2024-10-19 19:32:01 +0300

    dynamic screen for windows

--- myportfolio/lib/main_page/static_column/static_column.dart
+++ myportfolio/lib/main_page/static_column/static_column.dart
@@ -22,26 +22,33 @@ class StaticColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    
     return Column(
+      
       crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
       children: [
         const SizedBox(height: 100),
         // Title Text at the top
-        Text(
-          'Roberts Kārlis Šmits',
-          style: theme.textTheme.displaySmall!.copyWith(
-            fontSize: 41,
-            fontWeight: FontWeight.w700,
-            wordSpacing: 9,
-            shadows: [
-              Shadow(
-                blurRadius: 2,
-                color: Colors.black.withOpacity(0.3),
-                offset: const Offset(2.0, 2.0),
-              ),
-            ],
+        //Padding(
+          //padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add padding
+          Text(
+            'Roberts Kārlis Šmits',
+            style: theme.textTheme.displaySmall!.copyWith(
+              fontSize: 41,
+              fontWeight: FontWeight.w700,
+              wordSpacing: 9,
+              shadows: [
+                Shadow(
+                  blurRadius: 2,
+                  color: Colors.black.withOpacity(0.3),
+                  offset: const Offset(2.0, 2.0),
+                ),
+              ],
+            ),
           ),
-        ),
+        //),
         const SizedBox(height: 10),
         Align(
           alignment: Alignment.centerLeft,
@@ -62,11 +69,10 @@ class StaticColumn extends StatelessWidget {
                   ),
                 ),
                 TextSpan(
-                  text:
-                      ' \u{2022} ', // Artistic separator, like a bullet or other symbol
+                  text: ' \u{2022} ', // Artistic separator
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: theme.colorScheme.primary, // Change as needed
+                    color: theme.colorScheme.primary,
                     fontSize: theme.textTheme.headlineSmall!.fontSize,
                     shadows: [
                       Shadow(
@@ -95,18 +101,21 @@ class StaticColumn extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20),
-        Text(
-          "I streamline and automate infrastructure, ensuring reliable, scalable, and efficient delivery of software.",
-          style: theme.textTheme.bodyMedium!.copyWith(
-            color: theme.colorScheme.inverseSurface,
-            fontWeight: FontWeight.w500,
-            shadows: [
-              Shadow(
-                blurRadius: 2,
-                color: Colors.black.withOpacity(0.3),
-                offset: const Offset(2.0, 2.0),
-              ),
-            ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add padding
+          child: Text(
+            "I streamline and automate infrastructure, ensuring reliable, scalable, and efficient delivery of software.",
+            style: theme.textTheme.bodyMedium!.copyWith(
+              color: theme.colorScheme.inverseSurface,
+              fontWeight: FontWeight.w500,
+              shadows: [
+                Shadow(
+                  blurRadius: 2,
+                  color: Colors.black.withOpacity(0.3),
+                  offset: const Offset(2.0, 2.0),
+                ),
+              ],
+            ),
           ),
         ),
         const SizedBox(height: 60),
@@ -117,89 +126,78 @@ class StaticColumn extends StatelessWidget {
           onProjectsPressed: onProjectsPressed,
           indicatorPosition: indicatorPosition,
         ),
-        const Spacer(),
+        const SizedBox(height: 20), // Added spacing instead of Spacer
         const Center(
           child: JokeSection(),
         ),
-        const Spacer(),
+        const SizedBox(height: 20), // Added spacing instead of Spacer
         // Contact Me and Social Links in Row
-        Row(
-          children: [
-            // LinkedIn and GitHub Icons
-            const IconLinkButton(
-              icon: FontAwesomeIcons.linkedin,
-              buttonColor: Color(0xFF0072B1),
-              url:
-                  'https://www.linkedin.com/in/roberts-k%C4%81rlis-%C5%A1mits-86134130b/',
-            ),
-            const IconLinkButton(
-              icon: FontAwesomeIcons.github,
-              buttonColor: Color(0xFF6e5494),
-              url: 'https://github.com/Nevelin-W',
-            ),
-            const Spacer(), // Pushes the email and chat button to the right
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add padding
+          
+          child: Row(
+            children: [
+              // LinkedIn and GitHub Icons
+              const IconLinkButton(
+                icon: FontAwesomeIcons.linkedin,
+                buttonColor: Color(0xFF0072B1),
+                url:
+                    'https://www.linkedin.com/in/roberts-k%C4%81rlis-%C5%A1mits-86134130b/',
+              ),
+              const IconLinkButton(
+                icon: FontAwesomeIcons.github,
+                buttonColor: Color(0xFF6e5494),
+                url: 'https://github.com/Nevelin-W',
+              ),
+              const Spacer(), // Pushes the email and chat button to the right
+              const SizedBox(width: 10),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton.icon(
+                  onPressed: () async {
+                    final Uri params = Uri(
+                      scheme: 'mailto',
+                      path: 'roberts365653@gmail.com',
+                      query: 'subject=Let\'s Chat&body=Hello, Roberts Kārlis Šmits!',
+                    );
 
-            const SizedBox(width: 8),
-            // Text(
-            //   'roberts365653@gmail.com',
-            //   style: theme.textTheme.bodyMedium!.copyWith(
-            //     color: theme.colorScheme.inverseSurface,
-            //     fontWeight: FontWeight.w500,
-            //     shadows: [
-            //       Shadow(
-            //         blurRadius: 2,
-            //         color: Colors.black.withOpacity(0.3),
-            //         offset: const Offset(2.0, 2.0),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            const SizedBox(width: 10),
-
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton.icon(
-                onPressed: () async {
-                  final Uri params = Uri(
-                    scheme: 'mailto',
-                    path: 'roberts365653@gmail.com',
-                    query:
-                        'subject=Let\'s Chat&body=Hello, Roberts Kārlis Šmits!',
-                  );
-
-                  // Check if the URL can be launched
-                  if (await canLaunchUrl(params)) {
-                    await launchUrl(params);
-                  } else {
-                    // Handle the error here
-                    throw 'Could not launch $params';
-                  }
-                },
-                icon: Icon(
-                  FontAwesomeIcons.solidEnvelope,
-                  color: theme.colorScheme.primary,
-                ),
-                label: Text(
-                  "Let's Chat",
-                  style: theme.textTheme.bodyMedium!.copyWith(
-                    fontWeight: FontWeight.w500,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 2,
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(2.0, 2.0),
-                      ),
-                    ],
+                    // Check if the URL can be launched
+                    if (await canLaunchUrl(params)) {
+                      await launchUrl(params);
+                    } else {
+                      // Handle the error here
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Could not launch email client.'),
+                        ),
+                      );
+                    }
+                  },
+                  icon: Icon(
+                    FontAwesomeIcons.solidEnvelope,
+                    color: theme.colorScheme.primary,
+                  ),
+                  label: Text(
+                    "Let's Chat",
+                    style: theme.textTheme.bodyMedium!.copyWith(
+                      fontWeight: FontWeight.w500,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 2,
+                          color: Colors.black.withOpacity(0.3),
+                          offset: const Offset(2.0, 2.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                    iconColor: theme.colorScheme.onSurface, // Text color
+                    overlayColor: Colors.transparent, // Makes the overlay transparent
                   ),
                 ),
-                style: TextButton.styleFrom(
-                  iconColor: theme.colorScheme.onSurface, // Text color
-                  overlayColor:
-                      Colors.transparent, // Makes the overlay transparent
-                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ],
     );
